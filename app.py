9879d16132ec1d16efcb381982f1eb38535cb26d
argonaultes <gael@argonaultes.fr>
2025-06-12 13:20:41 +0200

    test event fixture

--- app.py
+++ app.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Welcome to ze BEFE'
+app.config.update({
+    "events": [
+    ]
+})
 
-@app.route('/events')
+
+@app.route("/")
+def index():
+    return "Welcome to ze BEFE"
+
+
+@app.route("/events")
 def events():
-    return render_template('events.html')
+    return render_template("events.html")
+
+
+@app.route("/event/<int:event_id>")
+def event(event_id=None):
+    result = list(
+        filter(lambda event: event["event_id"] == int(event_id), app.config.get("events"))
+    )
+    return render_template("event.html", event=next(iter(result), None))
--- test_app.py
+++ test_app.py
@@ -1,24 +1,54 @@
 import pytest
 from app import app as befe
 
+
 @pytest.fixture
 def app():
     befe.config.update(
         {
-            'TESTING': True
+            "TESTING": True,
+            "events": [
+                {
+                    "event_id": 1,
+                    "event_title": "event 1",
+                    "is_free": True,
+                    "already_registered": False,
+                },
+                {
+                    "event_id": 2,
+                    "event_title": "event 2",
+                    "is_free": True,
+                    "already_registered": True,
+                },
+                {
+                    "event_id": 3,
+                    "event_title": "event 3",
+                    "is_free": False,
+                    "already_registered": False,
+                },
+                {
+                    "event_id": 4,
+                    "event_title": "event 4",
+                    "is_free": False,
+                    "already_registered": True,
+                },
+            ],
         }
     )
     return befe
+
 
 @pytest.fixture
 def client(app):
     return app.test_client()
 
-def test_request_index(client):
-    response = client.get('/')
-    assert b'Welcome to ze BEFE' in response.data
 
-@pytest.mark.parametrize('event_id', list(range(1, 4)))
+def test_request_index(client):
+    response = client.get("/")
+    assert b"Welcome to ze BEFE" in response.data
+
+
+@pytest.mark.parametrize("event_id", list(range(1, 5)))
 def test_request_events(client, event_id):
-    response = client.get('/events')
-    assert 'event {event_id}'.format(event_id = event_id).encode() in response.data
+    response = client.get("/events")
+    assert "event {event_id}".format(event_id=event_id).encode() in response.data
